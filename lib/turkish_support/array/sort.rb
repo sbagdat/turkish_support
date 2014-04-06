22445296c28e6d97e81d95c30daa4cf58e48870d
Sıtkı Bağdat <sbagdat@gmail.com>
2014-04-06 21:03:35 +0300

    Fix Array#sort bug
    When type of items of array doesn't string, it causes an error.

--- lib/turkish_support/array/sort.rb
+++ lib/turkish_support/array/sort.rb
@@ -2,7 +2,11 @@ module TurkishSupport
   refine Array do
     def sort
       sort_by do |item|
+        if item.is_a?(String)
           item.chars.map { |ch| ORDERED_CHARS.index(ch) }
+        else
+          super
+        end
       end
     end
   end
