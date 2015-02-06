eb0566f194020417cc519240bab2229cc21fdc0c
Sıtkı Bağdat <sbagdat@gmail.com>
2015-02-06 03:48:09 +0200

    Release v0.3.0 - Bugfixes and support for new methods

--- lib/turkish_support.rb
+++ lib/turkish_support.rb
@@ -2,7 +2,7 @@ require "turkish_support/version"
 require "turkish_support/constants"
 require "turkish_support/string_helpers"
 require "turkish_support/string_methods"
-require "turkish_support/array/sort"
+require "turkish_support/array_methods"
 require "turkish_support/destructives"
 
 module TurkishSupport
