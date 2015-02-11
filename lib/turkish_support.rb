45cdf2f35ac10b44ebb6a3bdb46e6739823137c5
Sıtkı Bağdat <sbagdat@gmail.com>
2015-02-11 03:00:55 +0200

    Remove absolute paths

--- lib/turkish_support.rb
+++ lib/turkish_support.rb
@@ -1,9 +1,9 @@
-require './turkish_support/version'
-require './turkish_support/constants'
-require './turkish_support/helpers'
+require 'turkish_support/version'
+require 'turkish_support/constants'
+require 'turkish_support/helpers'
 
-require './turkish_support/string_methods'
-require './turkish_support/array_methods'
+require 'turkish_support/string_methods'
+require 'turkish_support/array_methods'
 
 module TurkishSupport
 end
