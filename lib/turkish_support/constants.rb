c0c9fe2d781459692ae841c5ea09d48b3797e750
Sıtkı Bağdat <sbagdat@gmail.com>
2020-11-06 06:04:39 +0300

    Remove unnecessary constants

--- lib/turkish_support/constants.rb
+++ lib/turkish_support/constants.rb
@@ -1,56 +1,38 @@
+# frozen_string_literal: true
+
 module TurkishSupportHelpers
-  ALPHA = {
-    lower:    'abcçdefgğhıijklmnoöpqrsştuüvwxyz',
-    upper:    'ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVWXYZ',
-    tr_lower: 'çğıiöşü',
-    tr_upper: 'ÇĞIİÖŞÜ'
-  }.freeze
-
+  ALPHA = { lower: 'abcçdefgğhıijklmnoöpqrsştuüvwxyz',
+      upper: 'ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVWXYZ',
+      tr_lower: 'çğıiöşü',
+      tr_upper: 'ÇĞIİÖŞÜ' }.freeze
   ALPHABET = ALPHA[:upper] + ALPHA[:lower]
-  
-  ASCII_ALPHABET = ALPHABET.chars.map.with_index do |ch, i|
-    # Add 65 to put special chars and numbers in correct order
-    [ch, i + 65]
-  end.to_h
-  
-  META_CHARS = {
-    '\w' => '[\p{Latin}\d_]',
-    '\W' => '[^\p{Latin}\d_]'
-  }.freeze
+  ASCII_ALPHABET = ALPHABET.chars.map.with_index { |ch, i| [ch, i + 65] }.to_h
+  META_CHARS = { '\w' => '[\p{Latin}\d_]', '\W' => '[^\p{Latin}\d_]' }.freeze
 
-  # Regexp required methods
-  RE_RE_METHS = %i(match scan).freeze
+  CASE_RELATED_METHS = %i[downcase
+                          downcase!
+                          upcase
+                          upcase!
+                          capitalize
+                          capitalize!].freeze
 
-  # Regexp optional methods
-  RE_OP_METHS = %i(
-    []
-    []=
-    =~
-    index
-    rindex
-    partition
-    rpartition
-    slice
-    slice!
-    split
-    sub
-    sub!
-    gsub
-    gsub!
-  ).freeze
+  REGEX_REQUIRED_METHODS = %i[match scan].freeze
+  REGEX_OPTIONAL_METHODS = %i[[]
+                              []=
+                              index
+                              rindex
+                              partition
+                              rpartition
+                              slice
+                              slice!
+                              split
+                              sub
+                              sub!
+                              gsub
+                              gsub!].freeze
+  REGEX_METHS = REGEX_REQUIRED_METHODS + REGEX_OPTIONAL_METHODS
+  RANGE_REGEXP = /\[[^\]]*?([#{ALPHABET}]-[#{ALPHABET}])[^\[]*?\]/.freeze
 
-  CASE_RELATED_METHS = %i(
-    downcase
-    downcase!
-    upcase
-    upcase!
-    capitalize
-    capitalize!
-  ).freeze
-
-  RANGE_REGEXP = /\[[^\]]*?([#{ALPHABET}]-[#{ALPHABET}])[^\[]*?\]/
-
-  CONJUCTIONS = %w(ve ile veya).freeze
-
-  SPECIAL_CHARS = %q{("'}.freeze
+  CONJUCTION = %w[ve ile veya].freeze
+  SPECIAL_CHARS = %q{("'}
 end
